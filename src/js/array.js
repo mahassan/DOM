0cae1e127e6da09cf95306fa95c1c3e1fca9d545
Ammar Hassan <ammar@ammarhassan.co.uk>
2022-02-03 05:34:56 +0000

    array method practice

---
+++ src/js/array.js
@@ -0,0 +1,13 @@
+const arr = ["laptop", "notebook", "pen","medicine","bidet"]
+// remove the last time
+const pop = arr.pop();
+
+console.log("After popping ",arr); //
+
+arr.unshift(pop);
+
+console.log("After unshifting ",arr)
+
+arr.sort();
+
+console.log("Sorted Array", arr);
